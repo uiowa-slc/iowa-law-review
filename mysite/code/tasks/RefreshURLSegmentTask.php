9b9249cf0601838020085a471dd5a883c9c97040
Dustin Quam <dustinquam@me.com>
2015-12-13 19:32:28 -0600

    added a url segment task for data migration

---
+++ mysite/code/tasks/RefreshURLSegmentTask.php
@@ -0,0 +1,19 @@
+<?php
+
+class RefreshURLSegmentTask extends BuildTask {
+
+	protected $title       = 'Refresh contributor URL segments';
+	protected $description = 'Reload and update all URL segments';
+
+	protected $enabled = true;
+
+	function run($request) {
+		$contributors = Contributor::get();
+
+		foreach ($contributors as $contributor) {
+			$contributor->write();
+			echo "wrote ".$contributor->Name.".<br />";
+		}
+	}
+
+}
